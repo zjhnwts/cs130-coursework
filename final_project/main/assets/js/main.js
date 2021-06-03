74e8fdba8d2a8425048aa77035ec7241ec731a11
zjhnwts <82182465+zjhnwts@users.noreply.github.com>
2021-06-03 03:40:13 -0500

    Project Updated

--- final_project/main/assets/js/main.js
+++ final_project/main/assets/js/main.js
@@ -140,15 +140,20 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  slides[1].style.display = "active";
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
+	// slides[i].className = slides[i].className.replace(" active", "");
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  if (slides[0].style.display == "none" && slides[1].style.display == "none" && slides[2].style.display == "none") {
+	slides[0].style.display = "block";
+	dots[0].className += " active";
+  }
+//   slides[slideIndex-1].className += " active";
 }
